52a1569904e1abc5716d977bb6c500341ef6ab4b
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-01-30 18:50:45 +0100

    fixar fotter

--- index.php
+++ index.php
@@ -16,9 +16,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Digital Internet Solutions">
-    <?php
-    include "include_pages/head.php";
-    ?>
+
+    <link href="css/main.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900i" rel="stylesheet">
+
+    <!--======================================= JAVA SCRIPT ===========================-->
+    <!--JQuery-->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <!--Velocity -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.js" type="text/javascript"></script>
+    <script src="js/jquery.color.js"></script>
+
+    <!-- JavaScript -->
+    <script src="js/scripts.js"></script>
+    <script>
+        smoothScroll.init();
+    </script>
+    <script src="js/jquery.stellar.js"></script>
+    <script>
+        $(document).ready(function () {
+            $(window).stellar();
+        });
+    </script>
+    <!-- Smooth scoll-->
+    <script src="js/smooth-scroll.min.js"></script>
+    <!--Fade in-->
+    <script src="js/fade.in.js"></script>
+    <!--slide in-->
+    <script src="js/slide.in.js"></script>
+
+    <!-- Latest compiled and minified JavaScript for bootstrap-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+            crossorigin="anonymous"></script>
+
+    <!--icon-->
+    <link rel="shortcut icon" href="/img/logo/icontop.png" type="image/x-icon" />
+
 </head>
 <?php
 /* including the separate function file */
