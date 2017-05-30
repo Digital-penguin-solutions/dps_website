66f239605d6a884f20e32f77e86b7c2e59b6a9bc
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-30 15:22:20 +0200

    Fixing file and code structure

--- app/index.php
+++ app/index.php
@@ -9,13 +9,51 @@
 <body class="wrapper col-xs-12" id="page-top" class="index">
 <?php include "partials/nav.php"?>
 
-<header>
-
+<!-- Header slider -->
+<header class="container-fluid i_header">
+    <div class="row-fluid i_header">
+        <div class="col-xs-12 i_header_container">
+            <?php include "views/index_slider.php"?>
+        </div>
+    </div>
 </header>
 
-<section>
+<!-- Skills -->
+<section class="container-fluid i_skills">
+    <div class="row-fluid i_skills">
+        <div class="col-xs-12 i_skills_container">
 
+        </div>
+    </div>
 </section>
+
+<!-- portfolio -->
+<section class="container-fluid i_portfolio">
+    <div class="row-fluid i_portfolio">
+        <div class="col-xs-12 i_portfolio_container">
+
+        </div>
+    </div>
+</section>
+
+<!-- About -->
+<section class="container-fluid i_about">
+    <div class="row-fluid i_about">
+        <div class="col-xs-12 i_about_container">
+
+        </div>
+    </div>
+</section>
+
+<!-- Contact -->
+<section class="container-fluid i_contact">
+    <div class="row-fluid i_contact">
+        <div class="col-xs-12 i_contact_container">
+
+        </div>
+    </div>
+</section>
+
 
 <?php include "partials/footer.php" ?>
 </body>
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 //import everything
 var g               = require('gulp');
-var sass            = require('gulp-sass');
+var less            = require('gulp-less');
+var path            = require('path');
 var cleanCss        = require('gulp-clean-css');
 var prefix          = require('gulp-autoprefixer');
 var uglify          = require('gulp-uglify');
@@ -10,93 +11,130 @@ var imgmin          = require('gulp-imagemin');
 var watch           = require('gulp-watch');
 var plumber         = require('gulp-plumber');
 var removeComm      = require('gulp-remove-html-comments');
+var header          = require('gulp-header');
+var pkg             = require('./package.json');
 var clean           = require('gulp-clean');
 var httpProxy       = require('http-proxy');
 var browserSync     = require('browser-sync');
 var connect         = require('gulp-connect-php');
 
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
 //delete dist folder
 g.task('clean', function () {
-    return g.src('dist', {read: false})
+    console.log("Clean all files in build folder");
+    return g.src('build', {read: false})
         .pipe(clean());
 });
 
 //remove comments from php and html documents
 g.task('removeComm', function () {
-    return g.src(['app/**/*.php','app/**/*.html'])
-        .pipe(plumber())
+    return g.src('html/**/*.php')
         .pipe(removeComm())
-        .pipe(g.dest('dist/'));
+        .pipe(g.dest('build/'));
 });
 
 //minify html and php files
-g.task('minify', ['removeComm'] , function(){
-    return g.src(['app/**/*.php' ,'app/**/*.html'])
+g.task('minify', ['removeComm'] , function() {
+    return g.src(['html/**/*.php' ,'html/**/*.html'])
         .pipe(plumber())
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(g.dest('dist'));
+        .pipe(g.dest('build/'));
 });
 
-//convert scss to css and add prefixes
-g.task('compile-sass', function () {
-    return g.src('app/scss/app.scss')
+//less stuff
+g.task('compile-less', function () {
+    return g.src('html/less/*.less')
         .pipe(plumber())
-        .pipe(sass())
-        .pipe(prefix('last 2 versions'))
-        .pipe(g.dest('app/css'));
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(g.dest('html/css'));
+});
+
+// Compile LESS files from /less into /css
+g.task('compile-less', function() {
+    return g.src('html/less/app.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(g.dest('html/css'));
+});
+
+g.task('prefix', ['compile-less'], function () {
+    return g.src('html/css/app.css')
+        .pipe(prefix({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(g.dest('html/css/'));
 });
 
 //minify css files and run the sass function before
-g.task('css-build', ['compile-sass'], function() {
-    return g.src('app/css/*.css')
+g.task('css-build', ['prefix'], function() {
+    return g.src('html/css/*.css')
         .pipe(plumber())
         .pipe(cleanCss({compatibility: 'ie8'}))
-        .pipe(g.dest('dist/css'));
+        .pipe(g.dest('build/css'));
 });
 
 //creating app.js from all js files in the app folder
 g.task('concat-js-app',function () {
-    return g.src('app/js/app/*.js)')
+    return g.src('html/js/app/*.js')
         .pipe(plumber())
         .pipe(concat('app.js'))
-        .pipe(g.dest('app/js'))
+        .pipe(g.dest('html/js'))
 });
 
 //create impots.js from all js files in imporst folder
-g.task('concat-js-third-party' ,function () {
-    return g.src('app/js/third_party/*.js')
+g.task('concat-js-third-party',function () {
+    return g.src('html/js/third-party/*.js')
         .pipe(plumber())
-        .pipe(concat('third_party.js'))
-        .pipe(g.dest('app/js'))
+        .pipe(concat('third-party.js'))
+        .pipe(g.dest('html/js'))
 });
 
 //uglify all flies
-g.task('js-build', ['concat-js-app','concat-js-third-party'] , function () {
-    return g.src('app/js/*.js')
+g.task('js-build', ['concat-js-third-party', 'concat-js-app'] , function () {
+    return g.src('html/js/*.js')
         .pipe(plumber())
         .pipe(uglify())
-        .pipe(g.dest('dist/js'))
+        .pipe(g.dest('build/js'))
 });
 
-//copy img and .htaccess files to dist folder
+//copy img and .htaccess files to build folder
 g.task('copy', function() {
-    return g.src(['app/.htaccess', 'app/config.ini'])
+    return g.src('html/.htaccess')
         .pipe(plumber())
-        .pipe(g.dest('dist'));
+        .pipe(g.dest('build'));
 });
 
-//compress all images from app folder
+//compress all images from html folder
 g.task('imgmin', function () {
-    return g.src('app/img/**/*')
+    return g.src('html/img/**/*')
         .pipe(plumber())
         .pipe(imgmin())
-        .pipe(g.dest('dist/img'));
+        .pipe(g.dest('build/img'));
 });
 
+// watch for file changes and performs the different tasks
+g.task('dev-watch', function () {
+    g.watch('html/js/**/*.js',         ['concat-js-app','concat-js-third-party']);
+    g.watch('html/less/*.less',        ['prefix']);
+});
+
+//connect to php server and use browser sync
 g.task('connect-php', function () {
     connect.server({
-        port: 8079,
-        base: 'app',
+        port: 80,
+        base: 'html',
         open: false
     });
 
@@ -105,14 +143,14 @@ g.task('connect-php', function () {
 
     browserSync({
         notify: false,
-        port  : 8079,
+        port  : 80,
         server: {
-            baseDir   : ['app'],
+            baseDir   : ['html'],
             middleware: function (req, res, next) {
                 var url = req.url;
 
                 if (!url.match(/^\/(css)\//)) {
-                    proxy.web(req, res, { target: 'http://localhost:8079' });
+                    proxy.web(req, res, { target: 'http://localhost:80' });
                 } else {
                     next();
                 }
@@ -121,14 +159,14 @@ g.task('connect-php', function () {
     });
 
     g.watch([
-        'app/**/*.html',
-        'app/**/*.php',
-        'app/js/*.js',
-        'app/css/app.css'
+        'html/**/*.html',
+        'html/**/*.php',
+        'html/js/*.js',
+        'html/css/app.css'
     ]).on('change', reload);
 
-    g.watch('app/scss/**/*scss',     ['compile-sass']);
-    g.watch('app/js/**/*.js',        ['concat-js-app','concat-js-third-party']);
+    g.watch('html/less/**/*',          ['prefix']);
+    g.watch('html/js/**/*',            ['concat-js-app','concat-js-third-party']);
 });
 
 g.task('build',['clean'],function () {
@@ -136,4 +174,6 @@ g.task('build',['clean'],function () {
 });
 
 //run css tole to compile css
-g.task('default', ['compile-sass', 'concat-js-app','concat-js-third-party', 'connect-php']);
+g.task('default', ['prefix', 'concat-js-app','concat-js-third-party', 'connect-php']);
+
+g.task('dev', ['prefix', 'concat-js-third-party', 'concat-js-app', 'dev-watch']);
