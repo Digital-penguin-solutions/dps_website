b6bc234753ff3f878bd72e97dba91f353914aa54
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-31 09:25:46 +0200

    smal code cleanup update

---
+++ app/calculator.php
@@ -0,0 +1,14 @@
+<?php include "partials/head.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Digitalis Home</title>
+</head>
+
+<body class="wrapper col-xs-12" id="page-top" class="Calculator">
+
+
+
+</body>
+</html>
