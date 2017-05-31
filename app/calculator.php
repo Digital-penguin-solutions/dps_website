e471564388dc8a81d19521e12f06207455c40841
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-31 09:32:01 +0200

    calculator

--- app/calculator.php
+++ app/calculator.php
@@ -6,9 +6,27 @@
   <title>Digitalis Home</title>
 </head>
 
-<body class="wrapper col-xs-12" id="page-top" class="Calculator">
+<body class="wrapper col-xs-12" id="page-top" class="calculator">
+
+<section class="container-fluid estimate">
+  <div class="row-fluid estimate">
+    <div class="col-xs-10 col-xs-offset-1 estimate_container">
+
+      <div class="estimate_btn">
+        <button>Estimate a price</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="container-fluid calculator">
+  <div class="row-fluid calculator">
+    <div class="col-xs-10 col-xs-offset-1 calculator_container">
 
 
+    </div>
+  </div>
+</section>
 
 </body>
 </html>
