3c18fcc55055de2648d945ccc1661c003f5632dc
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-31 08:59:10 +0200

    mort porting from old site

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,3 +1,33 @@
-<footer>
-
+<!--footer-->
+<footer class="container-fluid item" id="Footer">
+    <div class="row-fluid">
+        <div class="fotter-styiling col-xs-12 ">
+            <a href="index">
+                <img class="col-md-6 col-md-offset-3 col-xs-10 col-xs-offset-1" src="img/logo/Logo-dark.svg" alt="Logo">
+            </a>
+            <div class="footer-top-text col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                <p class="col-md-4">Design</p>
+                <p class="col-md-4">Branding</p>
+                <p class="col-md-4">E-commerce</p>
+                <hr class="col-md-11">
+            </div>
+            <div class="footer-mid-text col-md-6 col-md-offset-3 col-xs-12">
+                <a data-skroll href="index#indexp3" class="col-xs-5">Latest work</a>
+                <a href="contact" class="col-xs-5">contact</a>
+                <hr class="col-xs-11">
+                <!--social media liks-->
+                <div class="footer-social col-xs-12">
+                    <a href="#" class="col-xs-2"><img src="img/footer/facebook.svg"></a>
+                    <a href="#" class="col-xs-2"><img src="img/footer/twitter.svg"></a>
+                    <a href="#" class="col-xs-2"><img src="img/footer/instagram.svg"></a>
+                    <a href="#" class="col-xs-2"><img src="img/footer/google-plus.svg"></a>
+                </div>
+            </div>
+            <div class="footer-bot-text col-md-6 col-md-offset-3">
+                <p class="col-md-12">ison is a web development company based in Gothemburg. </p>
+                <a href="terms-conditions" class="col-md-6">Terms and Condition </a>
+                <a href="Privacy-policy" class="col-md-6">Privacy Policy</a>
+            </div>
+        </div>
+    </div>
 </footer>
