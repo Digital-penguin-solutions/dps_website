6bdd0a3e51014d4329bcb085841cdaa1b663f77b
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-11-13 17:20:52 -0800

    Header Index done for bothe mobil and desktop don

--- app/views/i_Header.php
+++ app/views/i_Header.php
@@ -1,30 +1,17 @@
-<header id="Header" class="i_header">
-
-  <?php include "views/Slider_elements.php";?>
-
-  <div class="all_slider_container i_header_slider_container">
-
-    <!-- Slider 1 -->
-    <div class="i_header_slider slider_page">
-
-      <!-- Slider Text -->
-      <div class="i_header_slider_text_container">
-        <h1>Hello!</h1>
-        <h4>amfömaw fömwaöfm ölmwfö mwföm öfwm öwfm ölmf ömfwa ölfwm </h4>
-        <button> Learn more</button>
-      </div>
+<header id="header" class="i_header">
+  <div class="header_container">
+    <div class="header_text">
+      <h1>DIGITAL PENGUIN SOLUTIONS</h1>
+      <h4>Website & graphical development</h4>
+      <hr>
+      <h5>We are a website and graphical development company based in Sweden</h5>
     </div>
-
-    <!-- Slider 2 -->
-    <div class="i_header_slider slider_page">
-
-      <!-- Slider Text -->
-      <div class="i_header_slider_text_container">
-        <h1>Hello!</h1>
-        <h4>amfömaw fömwaöfm ölmwfö mwföm öfwm öwfm ölmf ömfwa ölfwm </h4>
-        <button> Learn more</button>
-      </div>
+    <div class="header_img_container slideshow">
+      <div class="slideshow-image" style="background-image: url('img/Sliders/Index/Header/1.jpg')"></div>
+      <div class="slideshow-image" style="background-image: url('img/Sliders/Index/Header/2.jpg')"></div>
+      <div class="slideshow-image" style="background-image: url('img/Sliders/Index/Header/3.jpg')"></div>
+      <div class="slideshow-image" style="background-image: url('img/Sliders/Index/Header/4.jpg')"></div>
+      <div class="slideshow-image" style="background-image: url('img/Sliders/Index/Header/5.jpg')"></div>
     </div>
-
   </div>
 </header>
