52841e1a5797cae29975e833b9d634c55690b83e
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-11-16 15:59:36 -0800

    services header done. services webdesign done

--- app/services.php
+++ app/services.php
@@ -5,17 +5,70 @@
 <html lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Digital penguin solutions Home</title>
+  <title>DPS SERVICES</title>
 </head>
 
 <body class="wrapper services">
 
+<!-- SERVICES header -->
 <section class="s_header">
   <div class="s_header_container">
     <div class="s_text_container">
       <h2>When the time comes to impress, we distinguish ourselves with the following talents. Curious about what we can offer you?</h2>
     </div>
-    <div class="s_header_links_container"></div>
+
+    <div class="s_header_links_container">
+
+      <a href="#webdesign" class="s_header_links">
+        <img src="img/Services/icon/webdesign.svg" alt="webdesign">
+        <p>Website / ui / ux</p>
+      </a>
+
+      <a href="#E-commerce" class="s_header_links">
+        <img src="img/Services/icon/E-commerce.svg" alt="webdesign">
+        <p>E-commerce solutions</p>
+      </a>
+
+      <a href="#SEO" class="s_header_links">
+        <img src="img/Services/icon/seo.svg" alt="webdesign">
+        <p>SEO optimization</p>
+      </a>
+
+      <a href="#Branding" class="s_header_links">
+        <img src="img/Services/icon/Branding.svg" alt="webdesign">
+        <p>Branding</p>
+      </a>
+
+    </div>
+  </div>
+</section>
+
+<section class="s_webdesign" id="webdesign">
+  <div class="s_webdesign_container">
+    <div class="s_webdesign_text">
+      <h3>We create webdesign with user-interface centerd aproche</h3>
+      <hr align="left" >
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse ac nisi dui. Quisque facilisis leo quis nulla ultrices,
+        id molestie ligula porttitor. Phasellus rhoncus scelerisque orci,
+        ac sollicitudin turpis consequat maximus. Suspendisse potenti.
+        Donec nec ex ac tortor interdum tristique vitae eget nibh.
+        Ut id ex ante. Nunc auctor consectetur nisi, ut blandit sem ultrices quis.
+        Ut blandit nulla et ipsum venenatis, a elementum velit consequat. Ut nisl risus,
+        congue sed elit vitae, euismod pretium nulla. Sed volutpat eros lacinia pretium rhoncus.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+        Cras auctor metus sit amet ante tristique rutrum. Curabitur blandit leo molestie augue ornare congue.
+        Quisque feugiat libero ut nulla posuere interdum.</p>
+    </div>
+
+    <div class="s_webdesign_img">
+      <div class="s_webdesign_frame">
+        <iframe src="http://www.tastaturen.se/" frameborder="0" allowfullscreen>
+
+        </iframe>
+      </div>
+    </div>
+
   </div>
 </section>
 
