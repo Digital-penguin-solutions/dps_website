3ef3c3c1b466c5049b620d2d5243602eebe7a287
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-11-16 17:27:49 -0800

    service page done

--- app/js/app/iframe.js
+++ app/js/app/iframe.js
@@ -1,3 +1,3 @@
 $('iframe').load(function() {
   $(this).contents().find('img').css({with: '100%', 'height': '100%'});
-})
+});
--- app/services.php
+++ app/services.php
@@ -29,20 +29,21 @@
         <p>E-commerce solutions</p>
       </a>
 
-      <a href="#SEO" class="s_header_links">
-        <img src="img/Services/icon/seo.svg" alt="webdesign">
-        <p>SEO optimization</p>
-      </a>
-
       <a href="#Branding" class="s_header_links">
         <img src="img/Services/icon/Branding.svg" alt="webdesign">
         <p>Branding</p>
+      </a>
+
+      <a href="#SEO" class="s_header_links">
+        <img src="img/Services/icon/seo.svg" alt="webdesign">
+        <p>SEO optimization</p>
       </a>
 
     </div>
   </div>
 </section>
 
+<!-- Web design -->
 <section class="s_webdesign" id="webdesign">
   <div class="s_webdesign_container">
     <div class="s_webdesign_text">
@@ -62,11 +63,94 @@
     </div>
 
     <div class="s_webdesign_img">
-      <div class="s_webdesign_frame">
-        <iframe src="http://www.tastaturen.se/" frameborder="0" allowfullscreen>
+      <div class="s_frame">
+        <iframe src="img/Services/Webdesign.png" frameborder="0">
 
         </iframe>
       </div>
+    </div>
+
+  </div>
+</section>
+
+<!-- E-comerce -->
+<section class="s_comerce">
+  <div class="s_comerce_container">
+
+    <div class="s_comerce_img">
+      <div class="s_frame">
+        <iframe src="img/Services/Webdesign.png" frameborder="0"></iframe>
+      </div>
+    </div>
+
+    <div class="s_comerce_text">
+      <h3>We create a unique experience that engages the shopper</h3>
+      <hr align="left" >
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse ac nisi dui. Quisque facilisis leo quis nulla ultrices,
+        id molestie ligula porttitor. Phasellus rhoncus scelerisque orci,
+        ac sollicitudin turpis consequat maximus. Suspendisse potenti.
+        Donec nec ex ac tortor interdum tristique vitae eget nibh.
+        Ut id ex ante. Nunc auctor consectetur nisi, ut blandit sem ultrices quis.
+        Ut blandit nulla et ipsum venenatis, a elementum velit consequat. Ut nisl risus,
+        congue sed elit vitae, euismod pretium nulla. Sed volutpat eros lacinia pretium rhoncus.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+        Cras auctor metus sit amet ante tristique rutrum. Curabitur blandit leo molestie augue ornare congue.
+        Quisque feugiat libero ut nulla posuere interdum.</p>
+    </div>
+
+  </div>
+</section>
+
+<!-- Branding -->
+<section class="s_brand">
+  <div class="s_brand_container">
+
+    <div class="s_brand_text">
+      <h3>We help you with your company identities by creating a unique brand experience.</h3>
+      <hr align="left" >
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse ac nisi dui. Quisque facilisis leo quis nulla ultrices,
+        id molestie ligula porttitor. Phasellus rhoncus scelerisque orci,
+        ac sollicitudin turpis consequat maximus. Suspendisse potenti.
+        Donec nec ex ac tortor interdum tristique vitae eget nibh.
+        Ut id ex ante. Nunc auctor consectetur nisi, ut blandit sem ultrices quis.
+        Ut blandit nulla et ipsum venenatis, a elementum velit consequat. Ut nisl risus,
+        congue sed elit vitae, euismod pretium nulla. Sed volutpat eros lacinia pretium rhoncus.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+        Cras auctor metus sit amet ante tristique rutrum. Curabitur blandit leo molestie augue ornare congue.
+        Quisque feugiat libero ut nulla posuere interdum.</p>
+    </div>
+
+    <div class="s_brand_img">
+      <img src="img/Services/branding.png" alt="branding">
+    </div>
+
+  </div>
+</section>
+
+<!-- SEO -->
+<section class="s_seo">
+  <div class="s_seo_container">
+
+    <div class="s_seo_img">
+      <img src="img/Services/seo.jpg" alt="branding">
+    </div>
+
+    <div class="s_seo_text">
+      <h3>Improve your search ranking with our strategic approach that works</h3>
+      <hr align="left" >
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse ac nisi dui. Quisque facilisis leo quis nulla ultrices,
+        id molestie ligula porttitor. Phasellus rhoncus scelerisque orci,
+        ac sollicitudin turpis consequat maximus. Suspendisse potenti.
+        Donec nec ex ac tortor interdum tristique vitae eget nibh.
+        Ut id ex ante. Nunc auctor consectetur nisi, ut blandit sem ultrices quis.
+        Ut blandit nulla et ipsum venenatis, a elementum velit consequat. Ut nisl risus,
+        congue sed elit vitae, euismod pretium nulla. Sed volutpat eros lacinia pretium rhoncus.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+        Cras auctor metus sit amet ante tristique rutrum. Curabitur blandit leo molestie augue ornare congue.
+        Quisque feugiat libero ut nulla posuere interdum.</p>
     </div>
 
   </div>
