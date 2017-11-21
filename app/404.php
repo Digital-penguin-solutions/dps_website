7b3d91e47c87a8aab033875c708c638055ada3d3
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-11-21 15:46:01 -0800

    404 page done

--- app/404.php
+++ app/404.php
@@ -1,13 +1,21 @@
-<!doctype html>
+<?php include "partials/head.php"; ?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Digital penguin solutions Home</title>
 </head>
-<body>
+
+<body class="wrapper index">
+
+<section class="error">
+  <div class="error_container">
+    <h2>Oh no, you look like you are lost.</h2>
+    <h3>Head back to our home page</h3>
+    <a href="index">Back home ></a>
+  </div>
+</section>
 
 </body>
 </html>
