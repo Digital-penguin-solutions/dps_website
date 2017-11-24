bd196e9db360adf9f780a167c02420debdba5016
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-11-24 13:35:02 -0800

    work_details problem section

--- app/work_details.php
+++ app/work_details.php
@@ -14,5 +14,33 @@
 <!-- Nav -->
 <?php //include "partials/nav.php"; ?>
 
+<section class="wd_head">
+  <div class="wd_head_container">
+
+  </div>
+</section>
+
+<section class="wd_problem">
+  <div class="wd_problem_container">
+
+    <div class="wd_problem_left">
+      <h3>The Challenge</h3>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis blandit magna,
+        a egestas elit aliquam vitae. Aenean pharetra dapibus metus, ac sollicitudin nulla commodo ac.
+        Sed velit eros, convallis sed rhoncus ac, eleifend eu tortor. Maecenas ut neque ut tellus aliquet pharetra.
+        Sed non lectus at orci placerat i</p>
+    </div>
+
+    <div class="wd_problem_right">
+      <h3>The solution</h3>
+     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis blandit magna,
+      a egestas elit aliquam vitae. Aenean pharetra dapibus metus, ac sollicitudin nulla commodo ac.
+      Sed velit eros, convallis sed rhoncus ac, eleifend eu tortor. Maecenas ut neque ut tellus aliquet pharetra.
+      Sed non lectus at orci placerat i</p>
+    </div>
+
+  </div>
+</section>
+
 </body>
 </html>
