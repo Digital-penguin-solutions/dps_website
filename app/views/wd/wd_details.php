760a169f25cab2e7991951cabaa76d42b01e8cda
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-12-07 15:38:53 -0800

    work_details head done

---
+++ app/views/wd/wd_details.php
@@ -0,0 +1,47 @@
+<section class="wd_details">
+  <div class="wd_details_container">
+
+    <div class="wd_details_Date">
+      <h4>Project Date</h4>
+      <hr>
+      <p>2015/11/10 - 2016/02/25</p>
+    </div>
+
+    <div class="wd_details_Lines">
+      <h4>Lines of code</h4>
+      <hr>
+      <p>35,209 Lines of code</p>
+    </div>
+
+    <div class="wd_details_Coffee">
+      <h4>Coffee Consumed</h4>
+      <hr>
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
+      <p>70L of coffee</p>
+    </div>
+
+
+    <div class="wd_details_Tags">
+      <h4>Tags</h4>
+      <hr>
+      <ul>
+        <li>PHP</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>PS</li>
+        <li>Ai</li>
+        <li>scss</li>
+      </ul>
+    </div>
+
+    <button>wist website</button>
+
+  </div>
+</section>
--- app/work_details.php
+++ app/work_details.php
@@ -11,122 +11,29 @@
 
 <body class="wrapper about">
 
-<!-- Nav -->
-<?php //include "partials/nav.php"; ?>
+<?php
+//include "partials/nav.php";
 
-<!-- work details header -->
-<section class="wd_head">
-  <div class="wd_head_container">
+// Head
+include "views/wd/wd_head.php";
 
-  </div>
-</section>
+//work problem and solution view
+include "views/wd/wd_problem.php";
 
-<!-- work problem and solution -->
-<section class="wd_problem">
-  <div class="wd_problem_container">
+// Desktop images
+include "views/wd/wd_desktop.php";
 
-    <div class="wd_problem_left">
-      <h3>The Challenge</h3>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis blandit magna,
-        a egestas elit aliquam vitae. Aenean pharetra dapibus metus, ac sollicitudin nulla commodo ac.
-        Sed velit eros, convallis sed rhoncus ac, eleifend eu tortor. Maecenas ut neque ut tellus aliquet pharetra.
-        Sed non lectus at orci placerat i</p>
-    </div>
+// Tablet images
+include "views/wd/wd_tablet.php";
 
-    <div class="wd_problem_right">
-      <h3>The solution</h3>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis blandit magna,
-        a egestas elit aliquam vitae. Aenean pharetra dapibus metus, ac sollicitudin nulla commodo ac.
-        Sed velit eros, convallis sed rhoncus ac, eleifend eu tortor. Maecenas ut neque ut tellus aliquet pharetra.
-        Sed non lectus at orci placerat i</p>
-    </div>
+// Mobile images
+include "views/wd/wd_mobile.php";
 
-  </div>
-</section>
-
-<!-- Desktop Images -->
-<section class="wd_desktop">
-
-  <img src="img/Work/Desktop/1.jpg" alt="Tastaturen.se index">
-  <img src="img/Work/Desktop/2.jpg" alt="test">
-  <img src="img/Work/Desktop/3.jpg" alt="test">
-  <img src="img/Work/Desktop/4.jpg" alt="test">
-
-</section>
-
-<!-- Tablet -->
-<section class="wd_tablet">
-
-    <div class="wd_tablet_container_1">
-      <img src="img/Work/Tablet/ipad_frame.png" alt="ipad frame" class="wd_tablet_frame">
-      <img src="img/Work/Tablet/index.JPG" alt="ipad inner frame" class="wd_tablet_inner">
-    </div>
-
-    <div class="wd_tablet_container_2">
-      <img src="img/Work/Tablet/ipad_frame.png" alt="ipad frame" class="wd_tablet_frame">
-      <img src="img/Work/Tablet/work_head_tablet.JPG" alt="ipad inner frame" class="wd_tablet_inner">
-    </div>
-
-</section>
-
-<!-- Mobile Slider -->
-<section class="wd_mobile">
-  <div class="wd_mobile_container">
-
-    <img class="wd_mobile_frame" src="img/Work/Mobile/Iphone.png" alt="asd">
-    <img class="wd_mobile_inner" src="img/Work/Mobile/1.jpg" alt="mobile 1">
-
-  </div>
-</section>
-
-<!-- Dun details about project -->
-<section class="wd_details">
-  <div class="wd_details_container">
-
-    <div class="wd_details_Date">
-      <h4>Project Date</h4>
-      <hr>
-      <p>2015/11/10 - 2016/02/25</p>
-    </div>
-
-    <div class="wd_details_Lines">
-      <h4>Lines of code</h4>
-      <hr>
-      <p>35,209 Lines of code</p>
-    </div>
-
-    <div class="wd_details_Coffee">
-      <h4>Coffee Consumed</h4>
-      <hr>
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <img src="img/Work/Icons/coffee.svg" alt="coffee icon">
-      <p>70L of coffee</p>
-    </div>
+// Details
+include "views/wd/wd_details.php";
 
 
-    <div class="wd_details_Tags">
-      <h4>Tags</h4>
-      <hr>
-      <ul>
-        <li>PHP</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>PS</li>
-        <li>Ai</li>
-        <li>scss</li>
-      </ul>
-    </div>
-
-    <button>wist website</button>
-
-  </div>
-</section>
+?>
 
 </body>
 </html>
